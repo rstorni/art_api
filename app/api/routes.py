d9ecb03fd1b09502d5bae3ff0f81707ff0dd49d4
RamiroStorni <ramirostorni@RamiroSornisMBP.lan>
2024-12-15 16:47:27 -0500

    adding openapi spec

--- app/api/routes.py
+++ app/api/routes.py
@@ -10,25 +10,8 @@ router = APIRouter()
 def index():
     return 'hello world'
 
-#Artists
-@router.get('/artists')
-def getArtists():
-    pass
-
-@router.get('/artist/{artist_id}')
-def getArtist():
-    pass
-
-@router.patch('/artist/{artist_id}')
-def updateArtist():
-    pass
-
-@router.post('/artist')
-def createArtist():
-    pass
-
 #Art Ojects
-@router.get('/artworks')
+@router.get('/art_objects')
 def getArtworks(db: Session = Depends(get_db)):
     artworks = db.query(model.ArtObject).all()
     for works in artworks:
